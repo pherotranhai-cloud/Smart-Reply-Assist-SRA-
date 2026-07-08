0abcbaa55e9f80d97f97d75964dbeb0ddc7f2407
pherotranhai-cloud <pherotranhai@gmail.com>
2026-07-09 00:14:08 +0700

    aaaa

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -55,8 +55,8 @@ export const SplashScreen: React.FC<SplashScreenProps> = ({ isDataLoaded, onComp
               transition={{ duration: 1.8, ease: "easeInOut" }}
             />
             <motion.text
-              x="900"
-              y="1800"
+              x="1200"
+              y="1200"
               textAnchor="middle"
               fill="#2FA05E"
               fontFamily="Arial, Helvetica, sans-serif"
